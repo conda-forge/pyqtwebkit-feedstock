4fd28634987a73d5aa35ca13d1c0c8b403286680
Ista Zahn <istazahn@gmail.com>
2022-08-05 07:30:35 -0400

    rebase using pyqt template

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,25 +1,19 @@
-%PYTHON% configure.py ^
-        --verbose ^
-        --confirm-license ^
-        --assume-shared ^
-        --qmake="%LIBRARY_BIN%\qmake.exe" ^
-        --bindir="%LIBRARY_BIN%" ^
-        --spec=win32-msvc ^
-        --disable QtNfc ^
-        --enable QtWebKit ^
-        --enable QtWebKitWidgets ^
-        --no-designer-plugin ^
-        --no-python-dbus ^
-        --no-qml-plugin ^
-        --no-qsci-api ^
-        --no-sip-files ^
-        --no-tools
+pushd pyqt
+copy LICENSE ..
 
-if errorlevel 1 exit 1
+set PATH=%cd%\jom;%PATH%
+set PATH=%LIBRARY_INC%;%PATH%
+
+call sip-build ^
+    --verbose ^
+    --confirm-license ^
+    --disable QtNfc ^
+    --no-make
+if %ERRORLEVEL% neq 0 exit 1
+
+pushd build
 
 jom
-if errorlevel 1 exit 1
-
-:: installing with jom seems to fail
-nmake install
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
+jom install
+if %ERRORLEVEL% neq 0 exit 1
