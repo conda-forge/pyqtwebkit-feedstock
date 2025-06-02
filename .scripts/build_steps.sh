ced2c558037400838ec88bb4f05c62035a6a0dfe
Sam Gillingham <gillingham.sam@gmail.com>
2025-06-02 15:00:40 +1000

    update to pyqt 5.15.11 and sync with pyqt recipe (#24)
    * update to pyqt 5.15.11 and sync with pyqt recipe
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.0, and conda-forge-pinning 2025.06.01.11.10.49
    
    * sync config with pyqt
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.0, and conda-forge-pinning 2025.06.01.11.10.49
    
    * sync yum_requirements
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.0, and conda-forge-pinning 2025.06.01.11.10.49
    
    * pin sip also just to be sure
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -54,16 +54,15 @@ ulimit -n 1024
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y alsa-lib expat gtk2 gtkmm24 libdrm libglvnd-egl libglvnd-glx libICE libselinux libSM libX11 libXau libxcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libxkbcommon libXrandr libXrender libXScrnSaver libXtst libXxf86vm mesa-dri-drivers mesa-libEGL mesa-libGL pciutils pcre xcb-util xcb-util-image xcb-util-keysyms xcb-util-renderutil xcb-util-wm xorg-x11-server-Xvfb
+/usr/bin/sudo -n yum install -y mesa-libGL mesa-libGL-devel mesa-libEGL-devel mesa-dri-drivers libX11-devel libglvnd-glx xorg-x11-server-Xvfb
 )
 
 # make the build number clobber
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
-if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]] && [[ "${HOST_PLATFORM}" != linux-* ]] && [[ "${BUILD_WITH_CONDA_DEBUG:-0}" != 1 ]]; then
+if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]] && [[ "${BUILD_WITH_CONDA_DEBUG:-0}" != 1 ]]; then
     EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --no-test"
 fi
-
 
 ( endgroup "Configuring conda" ) 2> /dev/null
 
